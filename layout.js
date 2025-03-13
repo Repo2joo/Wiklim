d5514c2a003fb24d1d08a5bf8e606a03a7142e0f
Duswnsey <hazer24879@gmail.com>
2025-03-14 01:37:39 +0900

    rc 지원

--- layout.js
+++ layout.js
@@ -2,7 +2,17 @@ import Link from "next/link"
 import "./css/wiklim.css"
 import SkinLicense from "./skinlicense"
 import Default from "@/defaultcomponents/default"
+import { useEffect, useState } from "react"
 export default function Skin (props) {
+    const [rc, setrc] = useState([])
+    useEffect(() => {
+        async function logic () {
+            const rc = await fetch(`${process.env.NEXT_PUBLIC_WIKI_URL}/api/recentchanges/20`)
+            const text = await rc.json();
+            setrc(text.recentchanges)
+        }
+        logic()
+    }, [])
     return (
     <div className="wiklim">
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -16,7 +26,7 @@ export default function Skin (props) {
             <div className="naventry dropdown"><i className="fas fa-wrench" /><span className="navbar-noticon"> 도구</span> ⋁
                 <div className="dropdown-box" style={{width:"190px"}}>
                     <div className="dropdown-entry"><i className="fas fa-comment" /> 최근 토론</div>
-                    <div className="dropdown-entry"><img style={{margin:0, padding:0, display:"inline"}} src="/skins/wiklim/img/isolation.png" width={16}/> 고립된 페이지</div>
+                    <div className="dropdown-entry"><img style={{margin:0, padding:0, display:"inline"}} src="/skins/Wiklim/img/isolation.png" width={16}/> 고립된 페이지</div>
                     <Link href={"/license"}><div className="dropdown-entry"><i className="fas fa-copyright" /> 라이선스</div></Link>
                     {props.data.user.isRegistered == true && (
                         props.data.user.permission.includes("admin") ||props.data.user.permission.includes("owner")) && (
@@ -58,26 +68,26 @@ export default function Skin (props) {
                                 <>
                                     {props.data.action != "watch" && (<Link className="content-tool-entry" href={`/w/${encodeURIComponent(props.data.namespace)}:${encodeURIComponent(props.data.title)}`}><i className="fas fa-file" /> 본문</Link>)}
                                     {props.data.action != "edit" && (<Link className="content-tool-entry" href={`/edit/${encodeURIComponent(props.data.namespace)}:${encodeURIComponent(props.data.title)}`}>{props.data.CanDoWithThisDoc.edit == true ? (<i className="fas fa-pen" />):(<i className="fas fa-skull" />)} 편집</Link>)}
-                                    {props.data.action != "history" && (<Link className="content-tool-entry" href={`/history/${encodeURIComponent(props.data.namespace)}/${encodeURIComponent(props.data.title)}`}><i className="fas fa-toilet-paper" /> 역사</Link>)}
+                                    {props.data.action != "history" && props.data.action != "historys" &&(<Link className="content-tool-entry" href={`/historys/${encodeURIComponent(props.data.namespace)}/${encodeURIComponent(props.data.title)}`}><i className="fas fa-toilet-paper" /> 역사</Link>)}
                                     {props.data.action != "acl" && (<Link className="content-tool-entry" href={`/acl/${encodeURIComponent(props.data.namespace)}/${encodeURIComponent(props.data.title)}`}><i className="fas fa-key" /> ACL</Link>)}
                                 </>
                             )}
                             </div>
                         
                     </div>
-                    <div dangerouslySetInnerHTML={{"__html":props.data.bodycontent}} />
+                    <div className="select" dangerouslySetInnerHTML={{"__html":props.data.bodycontent}} />
                     {props.data.hasdefault == true && (<Default action={props.data.action} />)}
                     {props.data.action == "license" && <SkinLicense />}
                 </div>
                 <div className="right-padding">
                     <div className="userbox">
                         <div className="userbox-info">
-                            <div className="userbox-profile"><img src="/skins/wiklim/img/userbox-user.wow.png" width={"60px"} height={"60px"}/></div>
+                            <div className="userbox-profile"><img src="/skins/Wiklim/img/userbox-user.wow.png" width={"60px"} height={"60px"}/></div>
                                 <div className="userbox-liner">
                                     <div className="userbox-job">
                                         {props.data.user.isRegistered == true ? (
                                             props.data.user.permission.includes(`owner`) ? ("서버장") : (
-                                                props.data.user.permission.includes(` admin `) ? ("관리자") : ("유저")
+                                                props.data.user.permission.includes(`admin`) ? ("관리자") : ("유저")
                                             )
                                         ) : ("IP")}
                                     </div>
@@ -86,6 +96,7 @@ export default function Skin (props) {
                         </div>
                         
                         <div className="userbox-buttons">
+                        <button className="userbox-button"><i className="fas fa-moon" /> 다크 테마로</button>
                             {props.data.user.isRegistered == false ? (
                                 <>
                                     <Link href={`/login/${encodeURIComponent(window.location.pathname)}`} className="userbox-button"><i className="fas fa-sign-in" /> 로그인</Link>
@@ -94,22 +105,16 @@ export default function Skin (props) {
                             ): (
                                 <>
                                     <Link href={`/preference`} className="userbox-button"><i className="fas fa-gear" /> 환경설정</Link>
+                                    <Link href={`/logout/${encodeURIComponent(window.location.pathname)}`} className="userbox-button"><i className="fas fa-door-open" /> 로그아웃</Link> {/*로그 you out!*/}
                                 </>
                             )}
-                            <button className="userbox-button"><i className="fas fa-moon" /> 다크 테마로</button>
                         </div>
                     </div>
                     <div className="live">
                         <div className="live-title"><i className="fas fa-sync" /> 최근변경</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
-                        <div className="live-entry">와우</div>
+                        {rc.map((entry) => {
+                            return (<Link href={`/w/${encodeURIComponent(entry.namespace)}:${encodeURIComponent(entry.title)}`} key={entry.modifiedtime} className="live-entry">{entry.namespace}:{entry.title}</Link>)
+                        })}
                     </div>
                 </div>
             </div>
--- skinlicense.js
+++ skinlicense.js
@@ -2,10 +2,18 @@
 export default function SkinLicense () {
     return <>
     <h1>Wiklim Skin license</h1>
+    <span className="fas fa-check"> 자유롭게 수정 및 배포</span><br />
+    <span className="fas fa-check"> 영리 목적으로 사용 가능</span> <br />
+    <span className="fas fa-check"> 2차 배포 시, 소스 공개 의무 없음</span> <br />
+    <span className="fas fa-check"> 오픈소스 라이선스 파일 혹은, 페이지에 이 라이선스를 명시 안해도 됨</span><br />
+    <span className="fas fa-check"> 영리 목적으로 사용 가능</span> <br /><br />
+    <span className="fas fa-x"> 보증</span> <br />
+    <span className="fas fa-x"> img 폴더의 하위 파일들은 CCL의 영향을 받음</span> <br />
+    <span className="fas fa-x"> 2차 배포시 원본 이미지를 포함하지 않으면 img 폴더 안에 파일을 넣더라도 CCL의 영향을 받지 않음</span> <br />
 <pre>{`
 IGNORING UNDER, THIS LICENSE CAN BE CHANGED ANY TIME
 
-The MIT License (MIT)
+The MIT License (MIT) without 10th line
 
 Copyright (c) 2025 Duswnsey
 
@@ -15,9 +23,6 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
